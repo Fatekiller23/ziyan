46fad71e6e5e23b8154577da14d5a5e98044d439
piaoliu <441594700@qq.com>
2017-04-10 20:07:49 +0800

    add file setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='ziyan',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='a easy-to-use data collector with your device.',
+    packages=['ziyan'],
+    license='MIT',
+    install_require=['pendulum', 'logbook', 'redis', 'influxdb'],
+    entry_points={
+        'console_scripts': [
+            'ziyan_make=ziyan:main'
+        ]
+    }
+)
