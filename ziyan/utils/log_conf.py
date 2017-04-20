db7f0ebfb242a60d3cd53989571345872ce3801e
heathkang <3478325757@qq.com>
2017-04-20 10:20:56 +0800

    logbook package 1.str output 2.log file output

--- ziyan/utils/log_conf.py
+++ ziyan/utils/log_conf.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
-import os
 import sys
 
 from logbook import StreamHandler,RotatingFileHandler
 from logbook import set_datetime_format
+#from logbook.queues
+#from logbook.ticketing
 
 '''
 show local time in log line timestamp 
@@ -16,11 +17,11 @@ set_datetime_format('local')
 
 
 def setup_logger(conf):
-    '''
+    """
     setup logbook
-    :param conf:  config 
+    :param conf:  config [logging]
     :return: None
-    '''
+    """
     str_output = conf['str_output']
     str_level = conf['str_level']
     log_output = conf['log_output']
@@ -31,15 +32,30 @@ def setup_logger(conf):
     format_string = conf['format_string']
 
     if str_output:
-        StreamHandler(sys.stdout,level=str_level,format_string=format_string).push_application()
+        StreamHandler(sys.stdout,level=str_level,format_string=format_string,bubble=True).push_application()
 
-    elif log_output:
+    if log_output:
         RotatingFileHandler(logfile,mode='a',encoding='utf-8',level=log_level,\
                             format_string=format_string,delay=False,max_size=max_size,\
                             backup_count=backup_count,filter=None,bubble=True
                             ).push_application()
 
     return None
+
+
+if __name__ ==  '__main__':
+    from ziyan.utils.util import get_conf
+    from logbook import Logger
+    conf = get_conf('../text_file/configuration.toml')['logging']
+    setup_logger(conf)
+    log = Logger('test')
+    log.debug(conf)
+    log.debug('debug:test')
+    log.info('info:test')
+    log.notice('notice:test')
+    log.warn('warning:test')
+    log.error('error:test')
+    log.critical('critical:test')
 
 
 
