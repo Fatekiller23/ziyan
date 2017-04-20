f83c3020b4dd0fdd0ecbc459bf8e925a0067b31b
heathkang <3478325757@qq.com>
2017-04-20 11:01:27 +0800

    log book package add comments

--- ziyan/utils/log_conf.py
+++ ziyan/utils/log_conf.py
@@ -2,10 +2,8 @@
 
 import sys
 
-from logbook import StreamHandler,RotatingFileHandler
+from logbook import StreamHandler, RotatingFileHandler
 from logbook import set_datetime_format
-#from logbook.queues
-#from logbook.ticketing
 
 '''
 show local time in log line timestamp 
@@ -19,33 +17,34 @@ set_datetime_format('local')
 def setup_logger(conf):
     """
     setup logbook
-    :param conf:  config [logging]
+    :param conf:  toml[logging]
     :return: None
     """
-    str_output = conf['str_output']
-    str_level = conf['str_level']
-    log_output = conf['log_output']
-    log_level = conf['log_level']
-    logfile = conf['log_file']
-    backup_count = conf['backup_count']
-    max_size = conf['max_size']
-    format_string = conf['format_string']
-
-    if str_output:
-        StreamHandler(sys.stdout,level=str_level,format_string=format_string,bubble=True).push_application()
-
-    if log_output:
-        RotatingFileHandler(logfile,mode='a',encoding='utf-8',level=log_level,\
-                            format_string=format_string,delay=False,max_size=max_size,\
-                            backup_count=backup_count,filter=None,bubble=True
+    str_output = conf['str_output']  # console output
+    str_level = conf['str_level']  # choose console log level to print
+    log_output = conf['log_output']  # local log file output
+    log_level = conf['log_level']  # choose log file level to save
+    logfile = conf['log_file']  # local log file save position
+    backup_count = conf['backup_count']  # count of local log files
+    max_size = conf['max_size']  # size of each local log file
+    format_string = conf['format_string']  # log message format
+    # open console print
+    if str_output is True:
+        StreamHandler(sys.stdout, level=str_level, format_string=format_string, bubble=True).push_application()
+    # open local log file output
+    if log_output is True:
+        RotatingFileHandler(logfile, mode='a', encoding='utf-8', level=log_level, \
+                            format_string=format_string, delay=False, max_size=max_size, \
+                            backup_count=backup_count, filter=None, bubble=True
                             ).push_application()
 
     return None
 
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     from ziyan.utils.util import get_conf
     from logbook import Logger
+
     conf = get_conf('../text_file/configuration.toml')['logging']
     setup_logger(conf)
     log = Logger('test')
@@ -56,13 +55,3 @@ if __name__ ==  '__main__':
     log.warn('warning:test')
     log.error('error:test')
     log.critical('critical:test')
-
-
-
-
-
-
-
-
-
-
