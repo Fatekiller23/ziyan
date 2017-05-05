5ff43be1c0087bb46d07b7250b5f4d82ba49b789
piaoliu <441594700@qq.com>
2017-05-05 23:00:24 +0800

    增加每日自动关闭线程机制
    1. 每日凌晨0点，自动关闭 comander/checker/handler/sender 线程
    2. 更改 redis wrapper 使用的类，从 redis.StrictRedis 类中创建连接池，
       增加 socket_timeout 参数，默认为 1 秒

--- ziyan/lib/Watchdog.py
+++ ziyan/lib/Watchdog.py
@@ -14,7 +14,7 @@ def watchdog(*args):
     :param args: 
     :return: None
     """
-    threads_name = {thread.name for thread in args[0]}
+    threads_name = {thread.name for thread in args[0].values()}
     while True:
 
         threads = set()
@@ -30,8 +30,14 @@ def watchdog(*args):
             # 获取死去线程的实例集
             dead_threads = [thread for thread in args[1] if thread.name in dead_threads]
 
+            threads_set = dict()
+
             for thread in dead_threads:
-                threading.Thread(target=thread.work, args=(args[2],), kwargs={},
-                                 name='%s' % thread.name, daemon=True).start()
+                thread = threading.Thread(target=thread.work, args=(args[2],), kwargs={},
+                                          name='%s' % thread.name, daemon=True)
+                thread.start()
+                threads_set[thread.name] = thread
+
+            args[3].thread_set = threads_set
 
         time.sleep(10)
--- ziyan/script/manage.py
+++ ziyan/script/manage.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import ctypes
+import datetime
 import glob
+import inspect
+import os
 import sys
 import time
 import unittest
-import os
 from queue import Queue
 from threading import Thread
 
@@ -16,6 +19,10 @@ from ziyan.lib.Watchdog import watchdog
 from ziyan.tests import Test_conf
 from ziyan.utils.logbook_wrapper import setup_logger
 from ziyan.utils.util import get_conf
+
+
+class Maintainer:
+    pass
 
 
 def start():
@@ -50,19 +57,38 @@ def start():
     # 用于迭代
     workers = [commander, checker, handler, sender]
 
-    thread_set = set()
+    thread_set = dict()
 
     for worker in workers:
         thread = Thread(target=worker.work, args=(queue,), kwargs={},
                         name='%s' % worker.name, daemon=True)
         thread.start()
-        thread_set.add(thread)
+        thread_set[worker.name] = thread
 
-    Thread(target=watchdog, name='watchdog', args=(thread_set, workers, queue), daemon=True).start()
+    recorder = Maintainer()
+    recorder.thread_set = thread_set
+    recorder.date = datetime.date.today()
+    Thread(target=watchdog, name='watchdog', args=(thread_set, workers, queue, recorder), daemon=True).start()
+    return recorder
 
 
 def test():
     unittest.main(Test_conf, argv=sys.argv[1:])
+
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        """if it returns a number greater than one, you're in trouble,
+        and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
 
 
 if __name__ == '__main__':
@@ -70,9 +96,12 @@ if __name__ == '__main__':
     parse.add_argument('action', action='store')
     command = parse.parse_args().action
     if command == 'run':
-        start()
+        record = start()
         while True:
+            if record.date != datetime.date.today():
+                record.date = datetime.date.today()
+                for thread in record.thread_set.values():
+                    _async_raise(thread.ident, SystemExit)
             time.sleep(5)
-            pass
     elif command == 'test':
         test()
