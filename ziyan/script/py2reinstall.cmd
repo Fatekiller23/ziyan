f92e38ffd6367dfe0fc3f3674649f62dfadf28d0
Marshall Fate <mgsf@163.com>
2017-05-11 10:49:48 +0800

    更改重新安装脚本
    新的脚本用windows bat 命令

--- ziyan/script/py2reinstall.cmd
+++ ziyan/script/py2reinstall.cmd
@@ -1,7 +1,7 @@
 title py2reinstall-ziyan
 py -2 -m pip uninstall -y ziyan 1>nul
 cd ../..
-if exist build rm -r build
+if exist build rd /s /q build
 echo 'clean build/'
 py -2 setup.py install 1>nul
 echo 'py -2 reinstall good'
--- ziyan/script/reinstall.cmd
+++ ziyan/script/reinstall.cmd
@@ -1,7 +1,7 @@
 title reinstall-ziyan
 pip uninstall -y ziyan 1>nul
 cd ../..
-if exist build rm -r build
+if exist build rd /s /q build
 echo 'clean build/'
 python setup.py install 1>nul
 echo 'good'
