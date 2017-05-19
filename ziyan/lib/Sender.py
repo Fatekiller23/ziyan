0c9c7a2d72c5c539eed038f1ed8d9e2ea6d919a9
piaoliu <441594700@qq.com>
2017-05-19 13:26:46 +0800

    msgpack 放入redis分支

--- ziyan/lib/Sender.py
+++ ziyan/lib/Sender.py
@@ -77,14 +77,15 @@ class Sender:
         if self.enque_log_flag:
             log.info(log_str)
 
-        # for pack
-        tags = msgpack.packb(tags)
-        fields = msgpack.packb(fields)
-        measurement = msgpack.packb(measurement)
-        unit = msgpack.packb(unit)
-        timestamp = msgpack.packb(timestamp)
-
         if self.to_where == 'redis':
+
+            # for pack
+            tags = msgpack.packb(tags)
+            fields = msgpack.packb(fields)
+            measurement = msgpack.packb(measurement)
+            unit = msgpack.packb(unit)
+            timestamp = msgpack.packb(timestamp)
+
             self.db.script_load(self.lua_path)
             lua_info = self.db.enqueue(timestamp=timestamp, tags=tags,
                                        fields=fields, measurement=measurement, unit=unit)
