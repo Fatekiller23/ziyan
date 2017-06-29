c3bbeb383db7c9ea0ffffbf692c163d507c5c409
piaoliu <441594700@qq.com>
2017-06-29 20:06:54 +0800

    热修改配置文件和插件功能完成，思路如下： 程序启动： 1、遍历工作目录下的 *.py *.lua *.toml 文件 2、根据文件的修改时间生成 SHA，记录在 Maintainer 实例中 运行中： 1、每隔 5 秒查询一次当前生成的 SHA 值与记录的是否一致，如果不一致，则将监控的线程全部杀死 2、watchdog 检查到线程死去后，重新加载插件和配置文件，并重启线程

--- ziyan/lib/Watchdog.py
+++ ziyan/lib/Watchdog.py
@@ -38,8 +38,7 @@ def watchdog(*args):
 
             # 获取死去线程的实例集
             # 重新加载配置文件
-            a = args[1](True)
-            dead_threads = [thread for thread in a if thread.name in dead_threads]
+            dead_threads = [thread for thread in args[1](True) if thread.name in dead_threads]
 
             threads_set = dict()
 
@@ -78,6 +77,8 @@ class Maintainer:
 
     def protect(self):
         for threadname, singal in self.thread_signal.items():
+
+            # 判断是否存在线程僵死，如果存在，kill 掉
             if time.time() - singal > 1200:
                 try:
                     self._async_raise(self.thread_set[threadname].ident, SystemExit)
@@ -86,23 +87,51 @@ class Maintainer:
                 except Exception as e:
                     log.error('\nThere is something wrong')
 
+        # 判断文件是否更改，如果更改，将监控的所有线程 kill 掉
         if self.get_sha() != self.sha:
-            print(self.get_sha(), self.sha)
+            log.warning("\nFile changes, program restart")
+
             for thread in self.thread_set.values():
                 try:
                     self._async_raise(thread.ident, SystemExit)
                 except Exception as e:
                     log.error('\nThere is something wrong on sha')
-            log.warning("\nFile changes, program restart")
+
             self.sha = self.get_sha()
 
     def get_sha(self):
+        """
+        组成一个 元素为 tuple 的列表，tuple的值为 {文件路径, 文件的最后修改时间}，
+        根据这个列表生成一个唯一 SHA 值
+        :return: SHA 值
+        """
         data = {}
-        for dirpath, dirnames, filenames in os.walk(r'.'):
-            for file in filenames:
-                if file[file.rfind('.') + 1:] in ['py', 'lua', 'toml']:
-                    data[os.path.join(dirpath, file)] = os.stat(os.path.join(dirpath, file)).st_mtime
+        files = self.get_file_list()
+
+        for file in files:
+            if file[file.rfind('.') + 1:] in ['py', 'lua', 'toml']:
+                data[file] = os.stat(file).st_mtime
 
         data = sorted(data.items(), key=lambda d: d[0])
         sha = hashlib.sha1(str(data).encode())
         return sha.hexdigest()
+
+    @staticmethod
+    def get_file_list(l='.', filelist=list()):
+        """
+        递归遍历工作目录，获取目录下的配置文件、python 文件、lua 文件列表
+        :param l:
+        :param filelist:
+        :return: list
+        """
+        new_dir = l
+        if os.path.isfile(l):
+            filelist.append(l)
+        elif os.path.isdir(l):
+            for s in os.listdir(l):
+                # 如果需要忽略某些文件夹，使用以下代码
+                if s in ["logs", ".idea"]:
+                    continue
+                new_dir = os.path.join(l, s)
+                Maintainer.get_file_list(new_dir, filelist)
+        return filelist
